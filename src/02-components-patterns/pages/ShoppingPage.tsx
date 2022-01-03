9625e0dd00c7e1baf278564955cae58292a9f58c
BlackOnyxs <robinag2018@gmail.com>
2022-01-02 20:37:23 -0500

    Component Extend Styles - Ready

--- src/02-components-patterns/pages/ShoppingPage.tsx
+++ src/02-components-patterns/pages/ShoppingPage.tsx
@@ -1,4 +1,6 @@
-import { ProductCard } from '../components/index';
+import { ProductCard, ProductImage, ProductTitle, ProductsButtons} from '../components';
+
+import  '../styles/custom-styles.css'
 
 const product = {
     id: '1',
@@ -17,10 +19,34 @@ export const ShoppingPage = () => {
                 flexDirection: 'row',
                 flexWrap: 'wrap'
             }}>
-                <ProductCard product={ product }>
-                    <ProductCard.Image  />
-                    <ProductCard.Title />
-                    <ProductCard.Buttons />
+                <ProductCard  
+                    product={ product }
+                    className="bg-dark text-white">
+                    <ProductCard.Image className ="custom-image" />
+                    <ProductCard.Title className="text-bold" />
+                    <ProductCard.Buttons className="custom-buttons" />
+                </ProductCard>
+
+                <ProductCard 
+                    className="bg-dark text-white"
+                    product={ product }>
+                    <ProductImage className ="custom-image" />
+                    <ProductTitle className="text-bold"/>
+                    <ProductsButtons className="custom-buttons"/>
+                </ProductCard>
+
+                <ProductCard 
+                    product={ product }
+                    style={{
+                        backgroundColor: '#70D1F8'
+                    }}
+                >
+                    <ProductImage style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}/>
+                    <ProductTitle style={{ fontWeight: 'bold' }}/>
+                    <ProductsButtons style={{
+                        display: 'flex',
+                        justifyContent: 'end'
+                    }}/>
                 </ProductCard>
             </div>
         </div>
